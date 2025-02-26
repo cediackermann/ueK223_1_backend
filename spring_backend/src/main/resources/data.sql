25c5ac5f1e21f7290fd465908dad1081b6f034da
Lorisimbrogno <loris.imbrogno@edu.tbz.ch>
2025-02-26 16:46:59 +0100

    auth added

--- spring_backend/src/main/resources/data.sql
+++ spring_backend/src/main/resources/data.sql
@@ -39,15 +39,22 @@ VALUES ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c
        ('1d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
     ON CONFLICT DO NOTHING;
 
--- ASSIGN AUTHORITIES TO ROLES
 INSERT INTO role_authority (role_id, authority_id)
-VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'), -- DEFAULT, DEFAULT
-       ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'), -- ADMIN, USER_MODIFY
-       ('ab505c92-7280-49fd-a7de-258e618df074', '9a7b8c9d-0e1f-2345-6789-abcdef012345'), -- ADMIN, USER_PROFILE_CREATE
-       ('ab505c92-7280-49fd-a7de-258e618df074', '7e5f6a7b-8c9d-ef01-2345-6789abcdef01'), -- ADMIN, USER_PROFILE_READ
-       ('ab505c92-7280-49fd-a7de-258e618df074', '8f6a7b8c-9d0e-f123-4567-89abcdef0123'), -- ADMIN, USER_PROFILE_MODIFY
-       ('ab505c92-7280-49fd-a7de-258e618df074', '26ffc98a-0e2c-4bb2-a083-f1bef04b6699'), -- ADMIN, USER_PROFILE_LIST_READ
-       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab'), -- USER, USER_DELETE
-       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '7e5f6a7b-8c9d-ef01-2345-6789abcdef01'), -- USER, USER_PROFILE_READ
-       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '8f6a7b8c-9d0e-f123-4567-89abcdef0123') -- USER, USER_PROFILE_MODIFY
-    ON CONFLICT DO NOTHING;
+VALUES
+    -- Assign default authority to all roles
+    ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
+    ('ab505c92-7280-49fd-a7de-258e618df074', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
+
+    -- Admin authorities
+    ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'), -- USER_MODIFY
+    ('ab505c92-7280-49fd-a7de-258e618df074', '9a7b8c9d-0e1f-2345-6789-abcdef012345'), -- USER_PROFILE_CREATE
+    ('ab505c92-7280-49fd-a7de-258e618df074', '7e5f6a7b-8c9d-ef01-2345-6789abcdef01'), -- USER_PROFILE_READ
+    ('ab505c92-7280-49fd-a7de-258e618df074', '8f6a7b8c-9d0e-f123-4567-89abcdef0123'), -- USER_PROFILE_MODIFY
+    ('ab505c92-7280-49fd-a7de-258e618df074', '26ffc98a-0e2c-4bb2-a083-f1bef04b6699'), -- USER_PROFILE_LIST_READ
+
+    -- User authorities
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '7e5f6a7b-8c9d-ef01-2345-6789abcdef01'), -- USER_PROFILE_READ
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '8f6a7b8c-9d0e-f123-4567-89abcdef0123'); -- USER_PROFILE_MODIFY
+
+ON CONFLICT DO NOTHING;
