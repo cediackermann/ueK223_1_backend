6dc6f6d0b394c9f47368f3746e5073862ece834a
cediackermann <ackermanncedi@gmail.com>
2025-02-26 15:26:33 +0100

    implement logs for UserProfile

--- spring_backend/src/main/java/com/example/demo/domain/userprofile/UserProfile.java
+++ spring_backend/src/main/java/com/example/demo/domain/userprofile/UserProfile.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @Accessors(chain = true)
+@Log4j2
 public class UserProfile extends AbstractEntity {
 
     @Column(name = "address")
@@ -37,6 +39,36 @@ public class UserProfile extends AbstractEntity {
     @PostRemove
     public void calculateAge() {
         this.age = Period.between(birthDate, LocalDate.now()).getYears();
+    }
+
+    @PrePersist
+    public void logNewUserProfileAttempt() {
+        log.info("New user profile created with id: {}", this.getId());
+    }
+
+    @PreUpdate
+    public void logUserProfileUpdateAttempt() {
+        log.info("User profile with id: {} updated", this.getId());
+    }
+
+    @PreRemove
+    public void logUserProfileRemovalAttempt() {
+        log.info("User profile with id: {} removed", this.getId());
+    }
+
+    @PostPersist
+    public void logNewUserProfile() {
+        log.info("New user profile created with id: {}", this.getId());
+    }
+
+    @PostUpdate
+    public void logUserProfileUpdate() {
+        log.info("User profile with id: {} updated", this.getId());
+    }
+
+    @PostRemove
+    public void logUserProfileRemoval() {
+        log.info("User profile with id: {} removed", this.getId());
     }
 
     public UserProfile(UUID id, String address, LocalDate birthDate, String profilePictureUrl) {
